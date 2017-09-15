6a2ee3489c5a96fcc6c70743aae417b6ee8d885d
ryllarisantana <rsantana@morphus.com.br>
2017-09-15 00:02:08 -0300

    Ajustes!

--- desafio.js
+++ desafio.js
@@ -5,7 +5,7 @@ const api = axios.create({
 })
 
 api.get("/repos/seita-ifce/hello-js-v3/issues/2/comments").then(ret => {
-    const mylist = ret.data.filter(comment => (new Date(comment.created_at) < new Date('2017-09-15'))&& comment.body.includes('hellojs-s03e02'))
+    const mylist = ret.data.filter(comment => (comment.created_at < '2017-09-15T03:59:59Z') && comment.body.includes('hellojs-s03e02'))
     console.log("Comentou: ")
     mylist.forEach(c => {
         console.log(c.user.login)
